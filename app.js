dcddef7e6216ab7235dba626b26c8ead02a76738
Jnicol56 <133825516+Jnicol56@users.noreply.github.com>
2023-07-25 15:15:36 -0400

    1st-final draft

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("this is connected")
+
+const contactForm = document.getElementById("contactForm");
+contactForm.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+
+    const selectValue = document.getElementById("select").value;
+    const nameValue = document.getElementById("name").value;
+    const emailValue = document.getElementById("email").value;
+    const messageValue = document.getElementById("textarea").value;
+
+    console.log("Selected Topic:", selectValue);
+    console.log("Name:", nameValue);
+    console.log("Email:", emailValue);
+    console.log("Message:", messageValue);
+});
